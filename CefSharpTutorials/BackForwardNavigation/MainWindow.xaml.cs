047d3ec18e9fa33cd79d39932ef2abbff60d076c
cefsharptutorials <cefsharptutorials@gmail.com>
2017-07-13 13:57:43 +0300

    Add BackForwardNavigation project

---
+++ CefSharpTutorials/BackForwardNavigation/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+
+namespace BackForwardNavigation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChromiumWebBrowser_OnFrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            Dispatcher.BeginInvoke((Action) (() =>
+            {
+                AddressBox.Text = e.Url;
+                BackBtn.IsEnabled = Browser.CanGoBack;
+                NavigateBtn.IsEnabled = !string.IsNullOrWhiteSpace(AddressBox.Text);
+                ForwardBtn.IsEnabled = Browser.CanGoForward;
+            }));
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Browser.CanGoBack)
+            {
+                Browser.Back();
+            }
+        }
+
+        private void NavigateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(AddressBox.Text))
+            {
+                Browser.Address = AddressBox.Text;
+            }
+        }
+
+        private void ForwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Browser.CanGoForward)
+            {
+                Browser.Forward();
+            }
+        }
+    }
+}
