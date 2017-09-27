c939c39b5ff59dd562e12ed67b00b6f56e6b7da6
cefsharptutorials <cefsharptutorials@gmail.com>
2017-09-27 20:59:56 +0300

    Add InjectDotNetToJavaScript project

---
+++ CefSharpTutorials/InjectDotNetToJavaScript/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+using CefSharp.Wpf;
+
+namespace InjectDotNetToJavaScript
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ChromiumWebBrowser Browser;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Browser = new ChromiumWebBrowser();
+            Browser.RegisterJsObject("dotNetMessage", new DotNetMessage());
+
+            Browser.IsBrowserInitializedChanged += (sender, args) =>
+            {
+                if (Browser.IsBrowserInitialized)
+                {
+                    Browser.LoadHtml(File.ReadAllText("index.html"));
+                }
+            };
+
+            MainGrid.Children.Add(Browser);
+        }
+    }
+
+    public class DotNetMessage
+    {
+        public void Show(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
