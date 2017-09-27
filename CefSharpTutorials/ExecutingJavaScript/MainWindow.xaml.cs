6ddbeadc9075d15496b8bf7a6c5f39bfa1d8e48d
cefsharptutorials <cefsharptutorials@gmail.com>
2017-09-27 20:59:21 +0300

    Add ExecutingJavaScript project

---
+++ CefSharpTutorials/ExecutingJavaScript/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+
+namespace ExecutingJavaScript
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ExecuteJavaScriptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Browser.CanExecuteJavascriptInMainFrame && !string.IsNullOrWhiteSpace(ScriptTextBox.Text))
+            {
+                JavascriptResponse response = await Browser.EvaluateScriptAsync(ScriptTextBox.Text);
+
+                if (response.Result != null)
+                {
+                    MessageBox.Show(response.Result.ToString(), "JavaScript Result");
+                }
+            }
+        }
+    }
+}
