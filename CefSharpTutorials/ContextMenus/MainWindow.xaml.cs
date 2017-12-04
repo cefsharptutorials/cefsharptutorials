bac3951df9e2a64900da7fa4d8529d5223b84c2b
cefsharptutorials <cefsharptutorials@gmail.com>
2017-12-04 13:46:10 +0200

    Add ContextMenus project

---
+++ CefSharpTutorials/ContextMenus/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+
+namespace ContextMenus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BrowserView.MenuHandler = new SearchContextMenuHandler();
+        }
+    }
+
+    public class SearchContextMenuHandler : IContextMenuHandler
+    {
+        public void OnBeforeContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters,
+            IMenuModel model)
+        {
+            model.Clear();
+            model.AddItem(CefMenuCommand.Find, "Search it");
+            model.SetEnabledAt(0, !string.IsNullOrWhiteSpace(parameters.SelectionText));
+        }
+
+        public bool OnContextMenuCommand(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters,
+            CefMenuCommand commandId, CefEventFlags eventFlags)
+        {
+            if (commandId == CefMenuCommand.Find)
+            {
+                string searchAddress = "https://www.bing.com/search?q=" + parameters.SelectionText;
+                frame.ExecuteJavaScriptAsync($"window.open('{searchAddress}', '_blank')");
+                return true;
+            }
+
+            return false;
+        }
+
+        public void OnContextMenuDismissed(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+        {
+        }
+
+        public bool RunContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters,
+            IMenuModel model, IRunContextMenuCallback callback)
+        {
+            return false;            
+        }
+    }
+}
